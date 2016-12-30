1b0640fa2b55f024492aba3388fd1309b27be852
Modesto Figuereo Fernandez <figuereo.fernandez.modesto@gmail.com>
2016-12-30 10:46:20 -0400

    Initial commit
    Changes to be committed:
    	new file:   .gitignore
    	new file:   .idea/compiler.xml
    	new file:   .idea/copyright/profiles_settings.xml
    	new file:   .idea/gradle.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   .idea/runConfigurations.xml
    	new file:   app/.gitignore
    	new file:   app/build.gradle
    	new file:   app/proguard-rules.pro
    	new file:   app/src/androidTest/java/com/modestofiguereo/justjava/ExampleInstrumentedTest.java
    	new file:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/com/modestofiguereo/justjava/MainActivity.java
    	new file:   app/src/main/res/layout/activity_main.xml
    	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    	new file:   app/src/main/res/values-w820dp/dimens.xml
    	new file:   app/src/main/res/values/colors.xml
    	new file:   app/src/main/res/values/dimens.xml
    	new file:   app/src/main/res/values/strings.xml
    	new file:   app/src/main/res/values/styles.xml
    	new file:   app/src/test/java/com/modestofiguereo/justjava/ExampleUnitTest.java
    	new file:   build.gradle
    	new file:   gradle.properties
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle

---
+++ app/src/main/java/com/modestofiguereo/justjava/MainActivity.java
@@ -0,0 +1,44 @@
+package com.modestofiguereo.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(2);
+        displayPrice(2*5);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_textview);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+            * This method displays the given price on the screen.
+    */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_textview);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
